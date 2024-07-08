801781a44a46151c645d0a1fa8b28f93493e36e6
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-07-08 11:28:08 +0545

    Added a banner'

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,14 @@
+import Layout from "@/src/components/layout/Layout"
+import Banner from "@/src/components/pageComponents/Home/Banner"
+
 const Home = () => {
-	return "hi"
+	return <div>
+		<Layout>
+			<div>
+				<Banner />
+			</div>
+		</Layout>
+
+	</div>
 }
 export default Home 
